53b8b3b08e77fed73bf0dbfa26a7686e0bc51d75
Anatoliy <be@webdesk.(none)>
2009-11-09 23:03:23 +0300

    Fix categories in outlay controller

--- httpdocs/controller/outlay.php
+++ httpdocs/controller/outlay.php
@@ -4,6 +4,14 @@ class outlay_ctl extends crud_ctl {
 	
 	var $table = 'outlay';
 	
+	function __init() {
+		$this->tpl->add('outlay_categories', db_fetch_array('
+			SELECT id, name
+			FROM outlay_category',
+			'name', 'id'));
+		return parent::__init();
+	}
+
 	function index() {
 		$sql = '
 			SELECT outlay.*, c.name, user.name as author_name
@@ -23,7 +31,6 @@ class outlay_ctl extends crud_ctl {
 			$id = db_insert($this->table, $_POST);
 			$this->_relative_redirect('index');
 		}
-		$this->tpl->add('outlay_categories', db_fetch_array('SELECT id, name FROM outlay_category', 'name', 'id'));
 	}
 	
 	function edit() {
